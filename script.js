5a0bed0f1f48c3db79286c1ba96d969085d11370
Elias Stagg <38533721+Staggen@users.noreply.github.com>
2018-09-21 19:05:20 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,48 @@
 $(document).ready(() => {
-    // Initial page load trigger
+    function scrollHandler(pageId) {
+        var page = $('#' + pageId);
+        var pageStart = page.offset().top;
+        var pageJump = false;
+        
+        function scrollToPage() {
+            pageJump = true;
+            $('html, body').animate({
+                scrollTop: pageStart
+            }, {
+                duration: 1000,
+                complete: () => {
+                    pageJump = false;
+                }
+            });
+        }
+        window.addEventListener('wheel', function(event) {
+            var viewStart = $(window).scrollTop();
+            if(!pageJump){
+                var pageHeight = page.height();
+                var pageStopPortion = pageHeight / 8;
+                var viewHeight = $(window).height();
+                var viewEnd = viewStart + viewHeight;
+                var pageStartPart = viewEnd - pageStart;
+                var pageEndPart = (pageStart + pageHeight) - viewStart;
+                var canJumpDown = pageStartPart >= 0;
+                var stopJumpDown = pageStartPart > pageStopPortion;
+                var canJumpUp = pageEndPart >= 0;
+                var stopJumpUp = pageEndPart > pageStopPortion;
+                var scrollingForward = event.deltaY > 0;
+                if((scrollingForward && canJumpDown && !stopJumpDown) 
+                    ||(!scrollingForward && canJumpUp && !stopJumpUp)){
+                    event.preventDefault();
+                    scrollToPage();
+                }
+            } else {
+                event.preventDefault();
+            }
+        });
+    }
+    // "Don't question why it works, but embrace the fact that it does" - Einstein, probably
+    new scrollHandler('one'); 
+    new scrollHandler('two');
+    new scrollHandler('three');
+    new scrollHandler('four');
+    new scrollHandler('five');
 });
