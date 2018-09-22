57f4b101a3db5b5be5e554346f6ed305c6030f77
Elias Stagg <38533721+Staggen@users.noreply.github.com>
2018-09-22 22:08:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ $(document).ready(() => {
     var pageJump=false; // Used for full page slide animations
     // End of global variable(s)
 
+    // Setting page-to-top on load
+    window.addEventListener("load",function(evt){
+		evt.preventDefault();
+		$('html,body').animate({
+			scrollTop:$('.wrapper').offset().top
+		}, 10); // Make it short so it appears it's not an animation at all.
+    });
+    // End of page-to-top on load
+
     // Slide in nav on page load
     $("nav").css({
         "transform":"rotate(-90deg) translate(-100%, 0)",
@@ -50,8 +59,8 @@ $(document).ready(() => {
                 }
             });
             switchFunction(page[0].id); // Making the nav underline move as you scroll
-        }
-
+		}
+		
         window.addEventListener('wheel', function(event) { // Window event listener for scrolling
             var viewStart = $(window).scrollTop(); // The scrollTop() method sets or returns the vertical scrollbar position for the selected elements
             if(!pageJump){ // If it's not already scrolling
@@ -66,22 +75,45 @@ $(document).ready(() => {
                 var canJumpUp = pageEndPart >= 0; // Can do full page slide animation of pageEndPart is larger than or equal to 0
                 var stopJumpUp = pageEndPart > pageStopPortion; // Stop jump up after one element
                 var scrollingForward = event.deltaY > 0; // The deltaY property returns a positive value when scrolling down, and a negative value when scrolling up, otherwise 0.
-                if((scrollingForward && canJumpDown && !stopJumpDown) 
+                if((scrollingForward && canJumpDown && !stopJumpDown)
                     ||(!scrollingForward && canJumpUp && !stopJumpUp)){
                     event.preventDefault();
                     scrollToPage();
                 }
-            } else { // If it is already scrolling
+            }else{ // If it is already scrolling
                 event.preventDefault();
             }
-        });
+		});
+		window.addEventListener('keydown', function(event){ // Window event listener for arrow key scrolling #StillHasNoIdeaWhatHeIsDoing
+			var viewStart = $(window).scrollTop(); // The scrollTop() method sets or returns the vertical scrollbar position for the selected elements
+            if(!pageJump){ // If it's not already scrolling
+                var pageHeight = page.height(); // The height() method sets or returns the height of the selected elements.
+                var pageStopPortion = pageHeight/8; // No idea what this one does. Division number does not seem to matter, as long as it is not 0, or 1.
+                var viewHeight = $(window).height(); // Get full element height
+                var viewEnd = viewStart + viewHeight; // viewEnd = scrollbar-position + element height = exact coordinates for the end of the transition
+                var pageStartPart = viewEnd - pageStart; // pageStartPart = (scrollbar-position + element height) - (page element top coordinates) = exact coordinates for the top of the scroll animation
+                var pageEndPart = (pageStart + pageHeight) - viewStart; // pageEndPart = (coordinates of top of page-element + page height) - coordinates of vertical scrollbar for current page = ??? !{tired} || FIX
+                var canJumpDown = pageStartPart >= 0; // Can do full page slide animation if pageStartPart is larger than or equal to 0
+                var stopJumpDown = pageStartPart > pageStopPortion; // Stops jump down after one element
+                var canJumpUp = pageEndPart >= 0; // Can do full page slide animation of pageEndPart is larger than or equal to 0
+                var stopJumpUp = pageEndPart > pageStopPortion; // Stop jump up after one element
+                var keyEntry = event.keyCode; // event.keyCode returns the event keypress code. For arrow up that is 40, for arrow down it is 38.
+				if((keyEntry == 40 && canJumpDown && !stopJumpDown)
+                    ||(keyEntry == 38 && canJumpUp && !stopJumpUp)){
+                    event.preventDefault();
+                    scrollToPage();
+                }
+            }else{ // If it is already scrolling
+                event.preventDefault();
+            }
+		});
     }
 
     function switchFunction(data){ // Moves the nav underline and changes it's colors respective to the pages
         switch(data){
             case 0:
             case "one":
-                $(".underline").css({
+                $("nav div").css({
                     "transform":"translate(850%,0)",
                     "-webkit-transform":"translate(850%, 0)",
                     "-moz-transform":"translate(850%, 0)",
@@ -91,7 +123,7 @@ $(document).ready(() => {
                 break;
             case 1:
             case "two":
-                $(".underline").css({
+                $("nav div").css({
                     "transform":"translate(650%,0)",
                     "-webkit-transform":"translate(650%, 0)",
                     "-moz-transform":"translate(650%, 0)",
@@ -101,7 +133,7 @@ $(document).ready(() => {
                 break;
             case 2:
             case "three":
-                $(".underline").css({
+                $("nav div").css({
                     "transform":"translate(450%,0)",
                     "-webkit-transform":"translate(450%, 0)",
                     "-moz-transform":"translate(450%, 0)",
@@ -111,7 +143,7 @@ $(document).ready(() => {
                 break;
             case 3:
             case "four":
-                $(".underline").css({
+                $("nav div").css({
                     "transform":"translate(250%,0)",
                     "-webkit-transform":"translate(250%, 0)",
                     "-moz-transform":"translate(250%, 0)",
@@ -121,7 +153,7 @@ $(document).ready(() => {
                 break;
             case 4:
             case "five":
-                $(".underline").css({
+                $("nav div").css({
                     "transform":"translate(50%,0)",
                     "-webkit-transform":"translate(50%, 0)",
                     "-moz-transform":"translate(50%, 0)",
