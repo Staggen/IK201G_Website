7813511538e12131de1764f3443aa4489d333f25
Elias Stagg <38533721+Staggen@users.noreply.github.com>
2018-09-27 04:57:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -129,7 +129,14 @@ $(document).ready(() => {
     }
 
     function loadContent(page){
-        var checkVar=false;
+        var home = false;
+        var about = false;
+        var team = false;
+        var portfolio = false;
+        var contact = false;
+        var teamContentOne = false;
+        var teamContentTwo = false;
+        var teamContentThree = false;
         switch(page){
             case 0:
             case "home":
@@ -137,21 +144,69 @@ $(document).ready(() => {
                 break;
             case 1:
             case "about":
-                var url=`https://api.github.com/search/repositories?q=language:javascript&sort=stars&order=desc`;
-                fetch(url,{
-                    method:'get',
-                    headers:{
-                        // 'Access-Control-Allow-Origin':'*',
-                        'Content-Type':'application/vnd.github.v3+json'
+                if(!about){
+                    console.log("About: "+about);
+                    about=true; // Confirm that the about has been loaded
+                    var dateObject = new Date();
+                    var year = parseInt(dateObject.getUTCFullYear());
+                    var month = parseInt(dateObject.getUTCMonth())+1; // getUTCMonth() counts 0-11 by default, so that's why we add +1 at the end
+                    var day = parseInt(dateObject.getUTCDate());
+                    function pastWeek(){
+                        day = day - 7;
+                        if(day < 1){ // If day goes below 1, it hops back a month
+                            month--;
+                            if(month < 0){ // If month goes below 0, it hops back a year
+                                year--;
+                                month = month + 12; // Setting the new month value
+                            }
+                            switch(month){ // Setting the new day value depending on month
+                                case 2:
+                                    if(year % 4 == 0){ // If leap year
+                                        day = 29 + day;
+                                    }else{ // If not leap year
+                                        day = 28 + day;
+                                    }
+                                    break;
+                                case 4:
+                                case 6:
+                                case 9:
+                                case 11:
+                                    day = 30 + day;
+                                    break;
+                                case 1:
+                                case 3:
+                                case 5:
+                                case 7:
+                                case 8:
+                                case 10:
+                                case 12:
+                                    day = 31 + day;
+                                    break;
+                            }
+                        }
+                        if(month < 10){ // If month is less than 10, it will get a 0 infront of it (for API reasons) and be converted to a string
+                            month = 0 + month.toString();
+                        }
+                        return year + "-" + month + "-" + day;
                     }
-                })
-                .then((response) => {
-                    return response.json();
-                })
-                .then((myJSON) => {
-                    console.log(JSON.stringify(myJSON));
-                })
-                .catch(error => console.log(error));
+                    var url=`https://api.github.com/search/repositories?q=language:javascript+created:>${pastWeek()}&sort=stars&order=desc`;
+                    fetch(url,{
+                        method:'get',
+                        headers:{
+                            'Content-Type':'application/vnd.github.v3+json; charset=utf-8',
+                        },
+                        })
+                        .then((response) => { // Response is of type responseObject
+                            return response.json(); // Convert responseObject to JSONObject
+                        })
+                        .then((response) => { // Use JSONObject
+                            for(var i = 0;i < 5;i++){ // Loop 5 times (get the 5-top trends in javascript this week)
+                                $("#trend"+i).html(response.items[i].name); // Add the link to the list
+                                $("#trend"+i).attr("href",response.items[i].html_url); // Set the links href to the href of the github repository
+                            }
+                        })
+                    .catch(error => console.error(error));
+                }
                 break;
             case 2:
             case "team":
@@ -162,14 +217,15 @@ $(document).ready(() => {
                     bars refuse to animate/load in. They just appear as if they would always
                     have been there.
             */
-                $(".timer").css({ // Load the timer button
-                    "visibility":"visible",
-                    "opacity":"1",
-                    "transition":"2s",
-                    "transition-delay":"5.5s"
-                });
-                if(loadAvatars){
-                    loadAvatars=false; // Confirm initial load of avatars
+                if(!team){
+                    team=true; // Confirm that the team page has been loaded
+                    console.log("Team: "+team);
+                    $(".timer").css({ // Load the timer button
+                        "visibility":"visible",
+                        "opacity":"1",
+                        "transition":"2s",
+                        "transition-delay":"5.5s"
+                    });
                     memberOne();
                     setTimeout(memberTwo,10000);
                     setTimeout(memberThree,20000);
@@ -182,6 +238,8 @@ $(document).ready(() => {
                     var thirdContent=setInterval(() => {
                         setTimeout(memberThree,20000);
                     },30000);
+                }else{
+                    break;
                 }
                 break;
             case 3:
@@ -190,44 +248,52 @@ $(document).ready(() => {
                 break;
             case 4:
             case "contact":
-                // function itemLoad(selector,transitionTime,transitionDelay);
-                itemLoad("#contact .text p",            "1s","1.0s");
-                itemLoad("#contact .forms #name",       "2s","1.4s");
-                itemLoad("#contact .forms #email",      "2s","1.8s");
-                itemLoad("#contact .forms #main",       "2s","2.2s");
-                itemLoad("#contact .forms #submit",     "2s","2.6s");
-                itemLoad("#contact #map",               "2s","3.0s");
-                itemLoad("#contact ul li:nth-child(1)", "2s","3.5s");
-                itemLoad("#contact ul li:nth-child(2)", "2s","3.7s");
-                itemLoad("#contact ul li:nth-child(3)", "2s","3.9s");
-                itemLoad("#contact ul li:nth-child(4)", "2s","4.1s");
-                itemLoad("#contact ul li:nth-child(5)", "2s","4.3s");
+                if(!contact){
+                    contact=true; // Confirm that the contact page has been loaded
+                    console.log("Contact: "+contact);
+                    // function itemLoad(selector,transitionTime,transitionDelay);
+                    itemLoad("#contact .text p",            "1s","1.0s");
+                    itemLoad("#contact .forms #name",       "2s","1.4s");
+                    itemLoad("#contact .forms #email",      "2s","1.8s");
+                    itemLoad("#contact .forms #main",       "2s","2.2s");
+                    itemLoad("#contact .forms #submit",     "2s","2.6s");
+                    itemLoad("#contact #map",               "2s","3.0s");
+                    itemLoad("#contact ul li:nth-child(1)", "2s","3.5s");
+                    itemLoad("#contact ul li:nth-child(2)", "2s","3.7s");
+                    itemLoad("#contact ul li:nth-child(3)", "2s","3.9s");
+                    itemLoad("#contact ul li:nth-child(4)", "2s","4.1s");
+                    itemLoad("#contact ul li:nth-child(5)", "2s","4.3s");
+                }
                 break;
         }
         function memberOne(){
-            if(checkVar){ // If any content slide has been run before
+            if(teamContentOne){ // If any content slide has been run before
                 $(".preview1").hide(); // Hide it
                 switchRight(".preview1",100); // Move it to the right
                 setTimeout(() => {
                     $(".preview1").show(); // Show it again
                 },100);
+                switchCenter(".preview1",500); // Move right > center
+                console.log("Full cycle 1: "+teamContentOne);
             }
-            switchCenter(".preview1",500); // Move right > center
-            itemLoad("#avatar1",    "2s","1.0s"); // Load the avatar
-            // function barLoad(selector,width,transitionTime,transitionDelay);
-            barLoad("#bar1", "75%", "2s","2.0s");
-            barLoad("#bar2","100%", "2s","2.2s");
-            barLoad("#bar3", "50%", "2s","2.4s");
-            barLoad("#bar4","125%", "2s","2.6s");
-            barLoad("#bar5", "75%", "2s","2.8s");
-            // function itemLoad(selector,transitionTime,transitionDelay);
-            itemLoad("#bar1 p",     "1s","4.0s");
-            itemLoad("#bar2 p",     "1s","4.3s");
-            itemLoad("#bar3 p",     "1s","4.6s");
-            itemLoad("#bar4 p",     "1s","4.9s");
-            itemLoad("#bar5 p",     "1s","5.2s");
+            if(!teamContentOne){
+                itemLoad("#avatar1",    "2s","1.0s"); // Load the avatar
+                // function barLoad(selector,width,transitionTime,transitionDelay);
+                barLoad("#bar1", "75%", "2s","2.0s");
+                barLoad("#bar2","100%", "2s","2.2s");
+                barLoad("#bar3", "50%", "2s","2.4s");
+                barLoad("#bar4","125%", "2s","2.6s");
+                barLoad("#bar5", "75%", "2s","2.8s");
+                // function itemLoad(selector,transitionTime,transitionDelay);
+                itemLoad("#bar1 p",     "1s","4.0s");
+                itemLoad("#bar2 p",     "1s","4.3s");
+                itemLoad("#bar3 p",     "1s","4.6s");
+                itemLoad("#bar4 p",     "1s","4.9s");
+                itemLoad("#bar5 p",     "1s","5.2s");
+                console.log("Full load 1: "+!teamContentOne);
+            }
             switchLeft(".preview1",10000); // Move center > left
-            checkVar=true; // Confirm the function has been run
+            teamContentOne=true; // Confirm that slide-page one has been loaded
         }
         function memberTwo(){
             $(".preview2").hide(); // Hide it
@@ -236,20 +302,24 @@ $(document).ready(() => {
                 $(".preview2").show(); // Show it again
             },100);
             switchCenter(".preview2",500); // Move it right > center
-            itemLoad("#avatar2",    "2s","1.0s"); // Load the avatar
-            // function barLoad(selector,width,transitionTime,transitionDelay);
-            barLoad("#bar6","75%",  "2s","2.0s");
-            barLoad("#bar7","100%", "2s","2.2s");
-            barLoad("#bar8","50%",  "2s","2.4s");
-            barLoad("#bar9","125%", "2s","2.6s");
-            barLoad("#bar10","80%", "2s","2.8s");
-            //function itemLoad(selector,transitionTime,transitionDelay);
-            itemLoad("#bar6 p",     "1s","4.0s");
-            itemLoad("#bar7 p",     "1s","4.3s");
-            itemLoad("#bar8 p",     "1s","4.6s");
-            itemLoad("#bar9 p",     "1s","4.9s");
-            itemLoad("#bar10 p",    "1s","5.2s");
+            if(!teamContentTwo){
+                itemLoad("#avatar2",    "2s","1.0s"); // Load the avatar
+                // function barLoad(selector,width,transitionTime,transitionDelay);
+                barLoad("#bar6","75%",  "2s","2.0s");
+                barLoad("#bar7","100%", "2s","2.2s");
+                barLoad("#bar8","50%",  "2s","2.4s");
+                barLoad("#bar9","125%", "2s","2.6s");
+                barLoad("#bar10","80%", "2s","2.8s");
+                //function itemLoad(selector,transitionTime,transitionDelay);
+                itemLoad("#bar6 p",     "1s","4.0s");
+                itemLoad("#bar7 p",     "1s","4.3s");
+                itemLoad("#bar8 p",     "1s","4.6s");
+                itemLoad("#bar9 p",     "1s","4.9s");
+                itemLoad("#bar10 p",    "1s","5.2s");
+                console.log("Full load 2: "+!teamContentTwo);
+            }
             switchLeft(".preview2",10000); // Move it center > left
+            teamContentTwo=true; // Confirm that slide-page two has been loaded
         }
         function memberThree(){
             $(".preview3").hide(); // Hide it
@@ -258,27 +328,31 @@ $(document).ready(() => {
                 $(".preview3").show(); // Show it again
             },100);
             switchCenter(".preview3",500); // Move it right > center
-            itemLoad("#avatar3",    "2s","1.0s"); // Load the avatar
-            // function barLoad(selector,width,transitionTime,transitionDelay);
-            barLoad("#bar11","90%", "2s","2.0s");
-            barLoad("#bar12","110%","2s","2.2s");
-            barLoad("#bar13","80%", "2s","2.4s");
-            barLoad("#bar14","40%", "2s","2.6s");
-            barLoad("#bar15","70%", "2s","2.8s");
-            //function itemLoad(selector,transitionTime,transitionDelay);
-            itemLoad("#bar11 p",    "1s","4.0s");
-            itemLoad("#bar12 p",    "1s","4.3s");
-            itemLoad("#bar13 p",    "1s","4.6s");
-            itemLoad("#bar14 p",    "1s","4.9s");
-            itemLoad("#bar15 p",    "1s","5.2s");
+            if(!teamContentThree){
+                itemLoad("#avatar3",    "2s","1.0s"); // Load the avatar
+                // function barLoad(selector,width,transitionTime,transitionDelay);
+                barLoad("#bar11","90%", "2s","2.0s");
+                barLoad("#bar12","110%","2s","2.2s");
+                barLoad("#bar13","80%", "2s","2.4s");
+                barLoad("#bar14","40%", "2s","2.6s");
+                barLoad("#bar15","70%", "2s","2.8s");
+                //function itemLoad(selector,transitionTime,transitionDelay);
+                itemLoad("#bar11 p",    "1s","4.0s");
+                itemLoad("#bar12 p",    "1s","4.3s");
+                itemLoad("#bar13 p",    "1s","4.6s");
+                itemLoad("#bar14 p",    "1s","4.9s");
+                itemLoad("#bar15 p",    "1s","5.2s");
+                console.log("Full load 3: "+!teamContentThree);
+            }
             switchLeft(".preview3",10000); // Move it center > left
+            teamContentThree=true; // Confirm that slide-page three has been loaded
         }
         function switchRight(selector,transitionDelay){
             setTimeout(() => {
                 $(selector).removeClass("switchLeft");
                 $(selector).removeClass("switchCenter");
                 $(selector).addClass("switchRight");
-            },transitionDelay);   
+            },transitionDelay);
         }
         function switchCenter(selector,transitionDelay){
             setTimeout(() => {
